9d346aabcf9d0b0d43dce34066a8cc875ce8581d
entozhevlad <vladislavzarubin113@gmail.com>
2026-02-21 22:41:53 +0300

    Удален хардкод портов и хоста, в makefile добавлена передача .env.local для всех команд для их корректного запуска

--- src/app/main.py
+++ src/app/main.py
@@ -1,19 +1,16 @@
 import os
 
 import uvicorn
-from dotenv import load_dotenv
 
 from app.routers import all_routers
 from app.service import create_app
-
-load_dotenv(".env.local")
 
 app = create_app("Welcome to My FastAPI App")
 for router in all_routers:
     app.include_router(router)
 
-host = os.getenv("APP_HOST", "0.0.0.0")
-port = int(os.getenv("APP_PORT", "8000"))
+host = os.environ["APP_HOST"]
+port = int(os.environ["APP_PORT"])
 
 if __name__ == "__main__":
     uvicorn.run(app, host=host, port=port)
